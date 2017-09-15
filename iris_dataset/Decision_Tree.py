44c91c49f26100b9d0423ffbc28511f3cc903da9
Shawn Vega <shawn@theexperienceengine.com>
2017-09-14 21:58:19 -0700

    started data normilization for decision tree

--- iris_dataset/Decision_Tree.py
+++ iris_dataset/Decision_Tree.py
@@ -1,17 +1,33 @@
 import csv
+from sklearn import tree
 
 training_set = []
 validation_set = []
+type_map = dict()
+type_num = 0
+X = []
+Y = []
 
 
-with open('iris.data', newline='\n') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    row_num = 0
-    for row in spamreader:
-        row_num += 1
-        if row_num % 2 == 0:
-            print(row_num, 't: ', ', '.join(row))
-            training_set.append(row)
-        else:
-            print(row_num, 'v: ', ', '.join(row))
-            validation_set.append(row)
+def read_and_split_data():
+    with open('iris.data', newline='\n') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        row_num = 0
+        for row in spamreader:
+            row_num += 1
+            if not row[len(row)-1] in type_map:
+                type_map[row[len(row)-1]] = type_num
+                type_num += 1
+            if row_num % 2 == 0:
+                print(row_num, 't: ', ', '.join(row))
+                training_set.append(row)
+            else:
+                print(row_num, 'v: ', ', '.join(row))
+                validation_set.append(row)
+
+
+def train():
+    pass
+
+if __name__ == '__main__':
+    read_and_split_data()
